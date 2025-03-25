0925fcdd6d3eb977164a9c8978d607a34b79fcb3
pm <trey5040@gmail.com>
2025-03-24 21:47:36 -0400

    Update config.h

--- config.h
+++ config.h
@@ -228,7 +228,7 @@ static const Key keys[] = {
 	/* { MODKEY,			XK_F2,         spawn,                  {.v = (const char*[]){ "tutorialvids", NULL } } }, */
 	{ MODKEY,			XK_F3,         spawn,                  {.v = (const char*[]){ "displayselect", NULL } } },
 	{ MODKEY,			XK_F4,         spawn,                  SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
-	/* { MODKEY,			XK_F5,         xrdb,                   {.v = NULL } }, */
+	{ MODKEY,			XK_F5,         xrdb,                   {.v = NULL } },
 	/* { MODKEY,			XK_F6,         spawn,                  {.v = (const char*[]){ "torwrap", NULL } } }, */
 	/* { MODKEY,			XK_F7,         spawn,                  {.v = (const char*[]){ "td-toggle", NULL } } }, */
 	/* { MODKEY,			XK_F8,         spawn,                  {.v = (const char*[]){ "mailsync", NULL } } }, */
